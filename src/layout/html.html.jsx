d4ebcd71909c65dfccc10d53a89fff1ab906097f
Adenine <adenine.dev@gmail.com>
2022-05-10 21:57:15 -0400

    more updates

--- src/layout/html.html.jsx
+++ src/layout/html.html.jsx
@@ -7,7 +7,7 @@ export default props => (
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+      <link rel="icon" href="favicon.ico" type="image/x-icon" />
       <title>{props.title}</title>
       <Global
         styles={css`
